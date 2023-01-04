22886f224dc0e2c8a553985c157855fe3042886a
boss42wa <107799688+boss42wa@users.noreply.github.com>
2023-01-04 19:51:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // content: ["./src/input.css"],
+    content: ["./src/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
